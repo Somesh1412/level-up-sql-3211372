5f009672fc524b905536b547962cb2dcfa944794
Somesh Ramisetty <113796019+Somesh1412@users.noreply.github.com>
2024-01-25 12:20:49 +0000

    save

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,13 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+SELECT ReservationID, Customers.CustomerID, FirstName, LastName 
+FROM Reservations JOIN Customers 
+ON Reservations.CustomerID=Customers.CustomerID
+WHERE Customers.FirstName='Norby' 
+AND Reservations.Date>'2022-07-24';
+
+--Cid=64, Norby, Sleight
+DELETE FROM Reservations 
+WHERE ReservationID=2000;
