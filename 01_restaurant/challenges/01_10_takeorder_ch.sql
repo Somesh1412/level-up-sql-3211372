ce8bb22c780bad9977c09a9112250b962bc8fdd4
Somesh Ramisetty <113796019+Somesh1412@users.noreply.github.com>
2024-01-30 09:13:09 +0000

    .

--- 01_restaurant/challenges/01_10_takeorder_ch.sql
+++ 01_restaurant/challenges/01_10_takeorder_ch.sql
@@ -7,3 +7,26 @@
 -- 1 Tropical Blue Smoothie
 -- Delivery date and time: September 20, 2022 @ 2PM (14:00)
 -- There are no taxes or other fees.
+
+SELECT * FROM Customers 
+WHERE LastName='Hundey'; --CId=70
+
+INSERT INTO Orders (CustomerID, OrderDate)
+VALUES (70, CURRENT_TIMESTAMP);--1001
+
+SELECT DishID from Dishes 
+where Name='Mini Cheeseburgers' 
+OR Name='House Salad' 
+OR Name='Tropical Blue Smoothie';--4,7,20
+
+INSERT INTO OrdersDishes (OrderID, DishID)
+VALUES (1001, 4), (1001, 7), (1001, 20);
+
+SELECT * FROM Dishes JOIN OrdersDishes 
+ON Dishes.DishID=OrdersDishes.DishID 
+where OrdersDishes.OrderID=1001;
+
+SELECT Sum(Dishes.Price) FROM Dishes 
+JOIN OrdersDishes 
+ON Dishes.DishID=OrdersDishes.DishID 
+where OrdersDishes.OrderID=1001;
