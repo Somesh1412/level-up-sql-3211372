6c9133065fcfbdeb743e27d9475413fb096b5c2f
Somesh Ramisetty <113796019+Somesh1412@users.noreply.github.com>
2024-01-30 09:50:10 +0000

    .

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,11 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+SELECT * FROM Customers 
+WHERE FirstName='Cleo' AND LastName='Goldwater';
+
+UPDATE Customers 
+SET FavoriteDish=(SELECT DishID FROM Dishes 
+  where Name='Quinoa Salmon Salad')
+WHERE CustomerID=(SELECT CustomerID FROM Customers 
+  WHERE FirstName='Cleo' AND LastName='Goldwater');
--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,10 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+
+SELECT Customers.CustomerID, FirstName, LastName, Email, count(OrderID) as OrderCount 
+FROM Customers JOIN Orders 
+ON Customers.CustomerID=Orders.CustomerID
+GROUP BY Orders.CustomerID
+ORDER BY OrderCount DESC 
+LIMIT 6;
