9c4c200159b254fbea23dd094b2761ebf0c9fcc1
Somesh Ramisetty <113796019+Somesh1412@users.noreply.github.com>
2024-01-30 23:12:04 +0000

    .

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,23 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+SELECT count(Title) 
+FROM Books WHERE Title='Dracula';
+
+SELECT LoanID, PatronID, BookID, LoanDate, ReturnedDate
+FROM Loans WHERE BookID IN (12, 60, 73)
+ORDER BY ReturnedDate;
+
+SELECT count(Books.Title)
+FROM Books JOIN Loans on Books.BookID=loans.BookID
+WHERE Books.Title='Dracula' 
+AND Loans.ReturnedDate IS NULL;
+
+SELECT (SELECT count(Title) 
+FROM Books WHERE Title='Dracula') - 
+(SELECT count(Books.Title)
+FROM Books JOIN Loans 
+on Books.BookID=loans.BookID
+WHERE Books.Title='Dracula' 
+AND Loans.ReturnedDate IS NULL) 
+as Available_copies;
+
