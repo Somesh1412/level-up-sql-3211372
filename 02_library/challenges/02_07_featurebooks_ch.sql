e42f1266103a3d3832547403c40fea9cb4c562e8
Somesh Ramisetty <113796019+Somesh1412@users.noreply.github.com>
2024-01-31 09:54:49 +0000

    all done

--- 02_library/challenges/02_07_featurebooks_ch.sql
+++ 02_library/challenges/02_07_featurebooks_ch.sql
@@ -2,3 +2,11 @@
 
 -- Make a pick list of books published from 1890-1899 
 -- which are not currently checked out.
+SELECT Books.Title, Books.Published, Books.Author
+FROM Books
+WHERE Books.Published BETWEEN 1890 AND 1899 
+AND BookID NOT IN (SELECT BookID FROM Loans WHERE ReturnedDate ISNULL) 
+ORDER BY Books.Published DESC;
+
+
+
--- 02_library/challenges/02_08_bookstats_ch.sql
+++ 02_library/challenges/02_08_bookstats_ch.sql
@@ -2,6 +2,15 @@
 
 -- Report 1: Show how many titles were published 
 -- in each year.
-
+SELECT Published, COUNT(DISTINCT(Title)) as BookCount
+FROM Books 
+GROUP BY Published
+ORDER BY BookCount DESC;
 -- Report 2: Show the five books that have been
 -- checked out the most.
+SELECT Books.Title, Author, COUNT(LoanID) AS count
+FROM Loans JOIN Books 
+ON Loans.BookID=Books.BookID
+GROUP BY Books.Title, Author
+ORDER BY count DESC
+LIMIT 5;
